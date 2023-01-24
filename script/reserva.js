aa074002ca6b0160aba864a2b9f54b1870267698
edcabralc <cabralnet@gmail.com>
2023-01-23 22:11:15 -0300

    sript para popular html

--- script/reserva.js
+++ script/reserva.js
@@ -21,10 +21,112 @@ modal.addEventListener("click", (e) => {
 
 const showItens = (e) => {
     e.preventDefault();
+
     servicesContent.classList.toggle("hide");
 };
 
 buttonServices.addEventListener("click", showItens);
+buttonModal.addEventListener("click", () => {
+    populaModal();
+    console.log("modal");
+});
+// const itemRoom = document.querySelectorAll(".item-label");
+// const formReserva = document.querySelector("[data-reserva='form-reserva']");
+
+// const rooms = [
+//     {
+//         id: 1,
+//         title: "Standard",
+//         price: 190,
+//         description:
+//             "A acomodação standard é um quarto confortável e funcional, ideal para aqueles que procuram um lugar para descansar durante a viagem. O quarto possui uma cama queen size ou duas camas de solteiro, mesa de trabalho, TV a cabo, ar-condicionado e banheiro privativo.",
+//         image: "../public/assets/images/acomodacoes/standard/standard1.png",
+//     },
+//     {
+//         id: 2,
+//         title: "Master",
+//         price: 290,
+//         description:
+//             "A acomodação master é um quarto luxuoso e espaçoso, projetado para oferecer aos hóspedes o máximo conforto e privacidade. O quarto possui uma cama king size, sofá-cama, TV de tela  plana, ar-condicionado e banheiro privativo com banheira de hidromassagem.",
+//         image: "../public/assets/images/acomodacoes/master/master.jpg",
+//     },
+//     {
+//         id: 3,
+//         title: "Deluxe",
+//         price: 390,
+//         description:
+//             " A acomodação deluxe é um quarto de categoria superior, projetado para proporcionar aos hóspedes uma estadia luxuosa e                                                confortável. O quarto possui uma cama king size ou duas camas queen size, TV de tela plana, ar-condicionado, banheiro privativo com chuveiro de hidromassagem e uma varanda privativa com vista panorâmica para o mar.",
+//         image: "../public/assets/images/acomodacoes/deluxe/luxo1.jpg",
+//     },
+// ];
+
+// const populateItems = rooms.map(({title, price, description, image}) => {
+//     console.log(title, price);
+//     const room = `<div>
+//         <div class="flex">
+//             <span class="circle">
+//               <span class="checkmark"></span>
+//             </span>
+//             <h3>${title}</h3>
+//         </div>
+//         <p>${description}</p>
+//         <p class="preco">R$ ${price},00</p>
+//         </div>
+//         <img
+//             src="${image}"
+//             alt="${title}"
+//         />
+//         `;
+
+//     return room;
+// });
+
+// itemRoom.forEach((item, index) => {
+//     item.innerHTML += populateItems[index];
+// });
+
+// const getRoom = (description) => {
+//     const item = rooms.filter(({title}) => title === description);
+
+//     return item;
+// };
+// console.log(getRoom("Master"));
+
+// formReserva.addEventListener("change", (e) => {
+//     if (e.target.checked) {
+//         getRoom(e.target.value);
+//         const reserva = getRoom(e.target.value);
+//         console.log(reserva[0].price);
+//     }
+// });
+
+// const buttonServices = document.querySelector(".btn-add-servicos");
+// const servicesContent = document.querySelector(".add-servicos-content");
+// const buttonModal = document.querySelector(".btn");
+// const modal = document.querySelector(".modal-wrapper");
+
+// buttonModal.addEventListener("click", () => {
+//     modal.style.display = "flex";
+// });
+
+// modal.addEventListener("click", (e) => {
+//     // console.log(e.target);
+//     const className = ["modal-wrapper", "close-button"];
+//     const classNameOfClickedElement = e.target.classList[0];
+//     const shouldClose = className.some(
+//         (className) => className === classNameOfClickedElement
+//     );
+//     if (shouldClose) {
+//         modal.style.display = "none";
+//     }
+// });
+
+// const showItens = (e) => {
+//     e.preventDefault();
+//     servicesContent.classList.toggle("hide");
+// };
+
+// buttonServices.addEventListener("click", showItens);
 
 // +++++++++++++++++++++++++++JOse Edmar++++++++++++++++++++++++++++++
 
@@ -44,12 +146,12 @@ const carrinhoReserva = {
     total: 0,
 };
 
-//função para selecionar campos no HTML com JS
+// função para selecionar campos no HTML com JS
 const selecionar = (seletor) => document.querySelector(seletor);
 
 let data_set = ["data-checkout", "data-checkin", "data-qdtA", "data-qdtC"];
 
-//Pegar informações de reservas
+// Pegar informações de reservas
 function recuperaData() {
     let dataCheckin, dataCheckout, dataQdtA, dataQdtC;
     dataCheckin = selecionar("[data-checkin]").value;
@@ -74,14 +176,14 @@ function recuperaData() {
     }
 }
 
-//Adicionar evento ao selecionar campos minha reserva
+// Adicionar evento ao selecionar campos minha reserva
 data_set.forEach((item) => {
     let cli = selecionar(`[${item}]`);
     cli.addEventListener("change", recuperaData);
     // cli.addEventListener("focusout", recuperaData);
 });
 
-//Criar evento para tipos acomodações
+// Criar evento para tipos acomodações
 document.querySelectorAll(".container-titulos-acomodacao").forEach((e) => {
     e.onchange = function () {
         acomodacao();
@@ -92,14 +194,14 @@ document.querySelectorAll(".container-titulos-acomodacao").forEach((e) => {
 document.querySelectorAll(".add-servicos-content ul li input").forEach((e) => {
     e.onclick = function () {
         populaServico();
-        carrinhoReserva.total = totalAdicionais().toFixed(2)
+        carrinhoReserva.total = totalAdicionais().toFixed(2);
         selecionar("#total").innerText = `R$ ${totalAdicionais().toFixed(2)}`;
     };
 });
 
 // Preenher Objeto carrinhoReserva com dados dos serviços adicionais
 function populaServico() {
-    if ( selecionar("#cofre").checked) {
+    if (selecionar("#cofre").checked) {
         carrinhoReserva.cofre = 50;
     } else {
         carrinhoReserva.cofre = 0;
@@ -155,7 +257,7 @@ function totalAdicionais() {
             (carrinhoReserva.qtdAdulto + carrinhoReserva.qtdCrianca) * 390
         );
     } else {
-        carrinhoReserva.acomodacao = 'standard'
+        carrinhoReserva.acomodacao = "standard";
         return (
             carrinhoReserva.cafe +
             carrinhoReserva.cofre +
@@ -179,8 +281,23 @@ function populaReserva() {
     selecionar(".container-listas li:nth-of-type(4) span").innerText =
         carrinhoReserva.qtdAdulto + carrinhoReserva.qtdCrianca;
     selecionar("#total").innerText = `R$ ${totalAdicionais().toFixed(2)}`;
-    carrinhoReserva.total = totalAdicionais().toFixed(2)
+    carrinhoReserva.total = totalAdicionais().toFixed(2);
 }
+
+// test
+function populaModal() {
+    const test = (selecionar(".test").innerText = carrinhoReserva.acomodacao);
+    console.log(test);
+    // selecionar(".container-listas li:nth-of-type(2) span").innerText =
+    //     carrinhoReserva.dataCheckin;
+    // selecionar(".container-listas li:nth-of-type(3) span").innerText =
+    //     carrinhoReserva.dataCheckout;
+    // selecionar(".container-listas li:nth-of-type(4) span").innerText =
+    //     carrinhoReserva.qtdAdulto + carrinhoReserva.qtdCrianca;
+    // selecionar("#total").innerText = `R$ ${totalAdicionais().toFixed(2)}`;
+    // carrinhoReserva.total = totalAdicionais().toFixed(2);
+}
+// end test
 
 // Função para preencher objeto  com tipo de acomodação
 function acomodacao() {
@@ -198,11 +315,11 @@ function acomodacao() {
     }
 }
 
-
 let reserva = JSON.stringify(carrinhoReserva);
-// console.log(reserva);
+console.log(reserva);
 
-// document.querySelector('.container-listas li:nth-of-type(4) span').innerText = '4'
+document.querySelector(".container-listas li:nth-of-type(4) span").innerText =
+    "4";
 // Adicionar local
 // let servicoString = JSON.stringify(servico);
 // console.log(typeof servicoString);
